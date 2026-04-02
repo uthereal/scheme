4c2b9b28e74b2b628ca3ec7a26d2f62223e96e31
Gofish <gofish@example.com>
2026-04-01 22:58:44 -0600

    Fixing go embed eject

--- proto/embed.go
+++ proto/embed.go
@@ -2,5 +2,9 @@ package proto
 
 import "embed"
 
-//go:embed **/*.proto *.proto
+//go:embed *.proto
+//go:embed core/*.proto
+//go:embed postgres/*.proto
+//go:embed core/shared/*.proto
 var FS embed.FS
+
